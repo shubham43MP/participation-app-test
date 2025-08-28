029f2347964ac551c1e8801ba4f68b97150fffa3
shubham43MP <shubhamdixit19950321@gmail.com>
2025-08-28 17:46:52 +0530

    chore: update server port

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import logger from './utils/logger';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   logger.info(`🟢 Server running at ${PORT}`);
