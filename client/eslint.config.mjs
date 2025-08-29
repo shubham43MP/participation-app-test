f5089a1d55d0ca8866bd87a99e1449ec7906b50e
shubham43MP <shubhamdixit19950321@gmail.com>
2025-08-29 13:01:46 +0530

    chore: update eslint configuration

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,7 +1,32 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import reactPlugin from "eslint-plugin-react";
+
 export default [
   {
     files: ["**/*.{ts,tsx,js,jsx}"],
-    languageOptions: { ecmaVersion: "latest", sourceType: "module" },
     ignores: ["node_modules/", ".next/", "build/"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react: reactPlugin,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
 ];
